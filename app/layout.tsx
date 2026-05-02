82e7dd7b1ff3e1f136e4af40ca1913e185c54389
kxxxx-hi <59868398+kxxxx-hi@users.noreply.github.com>
2026-05-02 15:13:53 +0800

    Update metadata title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'The Etymologist',
-  description: 'Roots and Vibes',
+  title: 'Italian Crash Course',
+  description: 'MAGARI!',
 }
 
 export default function RootLayout({
